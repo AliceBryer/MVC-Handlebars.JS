91da17162c89e665b7dd757a41cad543139a9d16
Alice Bryer <alicebryer123@gmail.com>
2022-07-05 21:32:14 +0100

    serialization exercise complete

---
+++ 08-Stu_Data-Serialization/Unsolved/controllers/dish-routes.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const Dish = require('../models/Dish');
+
+router.get('/', async (req, res) => {
+ try {
+  const Dishes = await Dish.findAll({
+    raw:true, 
+  });
+
+  res.status(200).render("all", { Dishes });
+ } catch (error) {
+  res.status(500).json(error);
+ }
+  // TODO: Build out this route so that it serializes all of the dish objects that it receives. See the 'get' route below for a hint.
+});
+
+// route to get one dish
+router.get('/dish/:id', async (req, res) => {
+  try{ 
+      const dishData = await Dish.findByPk(req.params.id);
+      if(!dishData) {
+          res.status(404).json({message: 'No dish with this id!'});
+          return;
+      }
+      const dish = dishData.get({ plain: true });
+      res.render('dish', dish);
+    } catch (err) {
+        res.status(500).json(err);
+    };     
+});
+
+module.exports = router;
